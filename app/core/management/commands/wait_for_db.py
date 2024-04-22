ea31c7c14b8426c8c8b432becec79b47409e18fb
tgo-mas <talmeidasf@gmail.com>
2024-04-22 16:46:10 -0300

    Adicionado comando wait_for_db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,12 @@
 """
 Comando para o Django esperar o Banco de Dados inicializar
 """
+from time import sleep
+
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
@@ -10,4 +15,15 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        pass
+        """Ponto de entrada para o comando"""
+        self.stdout.write("Esperando o banco de dados...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Banco de dados iniciando, esperando 1 seg")
+                sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Banco de dados iniciado!"))
