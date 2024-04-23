dde42ab236620e6b0db2a29c60ef4d42d3b843a8
tgo-mas <talmeidasf@gmail.com>
2024-04-23 19:41:42 -0300

    Adicionando testes para usuário logado

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,21 +11,22 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
-    """Cria e retorna um novo usuário"""
+    """Cria e retorna um novo usuário."""
     return get_user_model().objects.create_user(**params)
 
 
 class PublicUserTests(TestCase):
-    """Testa os requisitos públicos de usuário"""
+    """Testa os requisitos públicos de usuário."""
 
     def setUp(self):
         self.client = APIClient()
 
     def test_criar_user_sucesso(self):
-        """Testa criar um usuário com caso de sucesso"""
+        """Testa criar um usuário com caso de sucesso."""
         payload = {
             'email': 'test@example.com',
             'password': 'exteste123',
@@ -41,7 +42,7 @@ class PublicUserTests(TestCase):
         self.assertNotIn('password', res.data)
 
     def test_user_email_ja_existe(self):
-        """Testa criar um usuário com caso de erro de email já existente"""
+        """Testa criar um usuário com caso de erro de email já existente."""
         payload = {
             'email': 'test@example.com',
             'password': 'exteste123',
@@ -53,7 +54,7 @@ class PublicUserTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_senha_muito_curta(self):
-        """Testa criar um usuário com erro de senha menor que 5 caracteres"""
+        """Testa criar um usuário com erro de senha menor que 5 caracteres."""
         payload = {
             'email': 'test@example.com',
             'password': 'ext2',
@@ -68,7 +69,7 @@ class PublicUserTests(TestCase):
         self.assertFalse(user_exists)
 
     def test_criar_token_user(self):
-        """Testa a criação de token de usuário"""
+        """Testa a criação de token de usuário."""
         user_details = {
             'email': 'user@example.com',
             'password': 'exteste123',
@@ -87,7 +88,7 @@ class PublicUserTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_criar_token_cred_invalidas(self):
-        """Testa requisitar um token com credenciais inválidas"""
+        """Testa requisitar um token com credenciais inválidas."""
         user_details = {
             'email': 'user@example.com',
             'password': 'goodpass'
@@ -104,7 +105,7 @@ class PublicUserTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_criar_token_sem_senha(self):
-        """Testa criação de token sem senha"""
+        """Testa criação de token sem senha."""
         payload = {
             'email': 'user@example.com',
             'password': '',
@@ -113,3 +114,52 @@ class PublicUserTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_recuperar_user_nao_autorizado(self):
+        """Testar que a autenticação é obrigatória."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserTests(TestCase):
+    """Testa as requisições para a API que requerem autenticação."""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@example.com',
+            password='exteste123',
+            name='Conta Teste'
+        )
+        self.client = APIClient()
+        self.client.force_authentication(user=self.user)
+
+    def test_recuperar_perfil_sucesso(self):
+        """Testa a recuperação do perfil logado."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_falha(self):
+        """Testa se o método POST falha na url Me."""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_atualizar_perfil(self):
+        """Testa a atualização do perfil do usuário."""
+        payload = {
+            'name': 'Nome Novo',
+            'password': 'novasenha123',
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
